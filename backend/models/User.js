2cb8b505ee88ec8ad0fe23e71c9aaf796281ea6c
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2023-02-23 00:38:54 +0530

    Backend Updated

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema= new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    DateOfJoin:{
+        type:Date,
+        default:Date.now
+    },
+    age:{
+        type:Number,
+        required:true,
+    },
+    gender:{
+        type:String,
+        required:true,
+    }
+})
+
+let user=mongoose.model('user', UserSchema)
+
+module.exports=user
