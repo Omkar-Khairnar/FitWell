27faa9b7bac6aaf60b1769bf7a9587e011dc2c7a
chiro-lytical <chirag.d21@iiits.in>
2023-03-22 17:46:59 +0530

    chatbox

--- App.js
+++ App.js
@@ -169,6 +169,9 @@ app.get('/user_Dashboard_profile', (req,res)=>{
 app.get('/user_dashboard_navbar', (req,res)=>{
     res.render('user_dashboard_navbar')
 })
+app.get('/user_dashboard_chat', (req,res)=>{
+    res.render('user_dashboard_chat')
+})
 app.get('/admin_dashboard_side_wrapper', (req,res)=>{
     res.render('admin_dashboard_side_wrapper')
 })
